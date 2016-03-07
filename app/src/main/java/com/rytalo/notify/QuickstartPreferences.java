f287556759640134b1d005bc0edbd3d5349ccf52
محمد <Mohamed.Hesham_taha20110377@yahoo.com>
2016-03-07 16:56:16 +0200

    Initial commit

---
+++ app/src/main/java/com/rytalo/notify/QuickstartPreferences.java
@@ -0,0 +1,12 @@
+package com.rytalo.notify;
+
+/**
+ * Created by محمد on 07/03/2016.
+ */
+
+public class QuickstartPreferences {
+
+    public static final String SENT_TOKEN_TO_SERVER = "sentTokenToServer";
+    public static final String REGISTRATION_COMPLETE = "registrationComplete";
+
+}
